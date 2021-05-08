1b1da3e88682b8f20bfe912b3e78d4e1132aa21d
tu <changweitu@gmail.com>
2021-05-08 23:16:55 +0800

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by tcw on 2021/5/8.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
